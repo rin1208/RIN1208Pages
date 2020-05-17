636b8bc7aad64610c41fc8a4f67f5e4c96bbc9c5
rin1208 <sora1208sky@gmail.com>
2020-05-17 20:43:42 +0900

    set

--- src/components/molecules/Profile.js
+++ src/components/molecules/Profile.js
@@ -4,6 +4,7 @@ import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import Icon from "../images/icon_mikisai.png";
 import styled from "@emotion/styled";
+import Link from "@material-ui/core/Link";
 
 const Hoge = styled.div`
   display: "flex";
@@ -12,6 +13,10 @@ const Hoge = styled.div`
 
 const Huga = styled.div`
   text-align: center;
+`;
+
+const Piyo = styled.div`
+  display: "flex";
 `;
 
 const Img = styled.img`
@@ -27,13 +32,13 @@ function Profile() {
       </Huga>
       <Hoge>
         <Grid container item xs={12} spacing={2}>
-          <Grid item xs={3}></Grid>
-          <Grid item xs={3}>
+          <Grid item xs={3} sm={0}></Grid>
+          <Grid item xs={3} sm={12}>
             <Huga>
               <Img src={Icon} alt="Icon" />
             </Huga>
           </Grid>
-          <Grid item xs={6}>
+          <Grid item xs={6} sm={12}>
             <h3>自己紹介</h3>
             <ul>
               <li>Name: RIN1208 (Backend Engineer)</li>
@@ -45,6 +50,11 @@ function Profile() {
                 主にサーバーサイドをメインに勉強していてGo等によく触れています
               </li>
             </ul>
+            <Piyo>
+              <Link href="https://twitter.com/sora1212su">Twitter</Link>
+              <Link href="https://github.com/rin1208">GitHub</Link>
+              <Link href="https://qiita.com/rin1208">Qiita</Link>
+            </Piyo>
           </Grid>
         </Grid>
       </Hoge>
