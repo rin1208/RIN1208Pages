1e207ee6a6ee868af88d692867d18e4102668050
rin1208 <sora1208sky@gmail.com>
2020-05-22 00:23:23 +0900

    set

--- src/components/molecules/Profile.js
+++ src/components/molecules/Profile.js
@@ -61,7 +61,7 @@ function Profile() {
                     <Link href="https://github.com/rin1208">GitHub</Link>
                   </Grid>
                   <Grid item xs={4} md={3}>
-                    <Link href="rin1208-blog.netlify.app">Blog</Link>
+                    <Link href="https://rin1208-blog.netlify.app/">Blog</Link>
                   </Grid>
                 </Grid>
               </Sub>
